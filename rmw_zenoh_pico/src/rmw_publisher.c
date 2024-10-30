5bc8c7fd95e21ffed24a2a83bb866db4287d44bc
Katsumi Yamazaki <v-yamazaki8668@esol.co.jp>
2024-10-30 14:17:19 +0900

    append esol Copyright for each files.

--- rmw_zenoh_pico/src/rmw_publisher.c
+++ rmw_zenoh_pico/src/rmw_publisher.c
@@ -1,4 +1,5 @@
 // Copyright 2019 Proyectos y Sistemas de Mantenimiento SL (eProsima).
+// Copyright(C) 2024 eSOL Co., Ltd.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
