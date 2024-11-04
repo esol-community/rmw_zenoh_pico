2ded326d99651364d412c9017240691d4ebf2e40
v-yamazaki8668 <v-yamazaki8668@esol.co.jp>
2024-11-04 06:51:42 +0000

    Apply 1 suggestion(s) to 1 file(s)

--- rmw_zenoh_pico/src/rmw_publisher.c
+++ rmw_zenoh_pico/src/rmw_publisher.c
@@ -151,7 +151,7 @@ bool declaration_publisher_data(ZenohPicoPubData *pub_data)
 					 z_move(token_callback_),
 					 NULL);
   if (!z_check(pub_data->token_)) {
-    RMW_ZENOH_LOG_DEBUG("Unable to declare tolken.");
+    RMW_ZENOH_LOG_DEBUG("Unable to declare token.");
     return false;
   }
 
